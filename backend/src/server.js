fe8f2d2986f1be5def512b3ea79f19a001dc3424
Divyanshu-hash <rishugiri056@gmail.com>
2026-06-05 00:02:34 +0530

    signup-frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 // const express = require('express');
 import { ENV } from "./lib/env.js";
 import express from 'express';
+import path from "path";
+import cors from "cors";
+
 import cookiesParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
@@ -13,7 +16,10 @@ const PORT= ENV.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
 app.use(cookiesParser());
+
 
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
